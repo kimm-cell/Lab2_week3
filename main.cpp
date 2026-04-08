922b0f75ce6372789b27655a106aaf04467afbee
kimm-cell <vivakgw@snu.ac.kr>
2026-04-08 14:09:09 +0900

    Add functionality to print combinations in func

--- main.cpp
+++ main.cpp
@@ -7,13 +7,24 @@ int N,M;
 vector<int> vec;
 
 void func(int cnt) {
+    if (cnt == M) {
+        for (int i = 0; i < M; i++) {
+            cout << vec[i];
+            if (i != M - 1) cout << " ";
+        }
+        cout << "\n";
+        return;
+    }
 
+    int start;
+    if (cnt == 0) start = 1;
+    else start = vec.back() + 1;
 
-
-
-
-
-
+    for (int i = start; i <= N; i++) {
+        vec.push_back(i);
+        func(cnt + 1);
+        vec.pop_back();
+    }
 }
 
 int main() {
